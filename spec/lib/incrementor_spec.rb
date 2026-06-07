f38c8efc9cebe0aea11be5b1ae43affa514906d3
Felipe Diesel <felipediesel@gmail.com>
2026-06-07 16:06:37 -0700

    Rewrite incrementor tests to verify behaviour through the public API
    - Replace stubbed private method tests with behavioural tests through #run
    - Remove all send() calls and mock expectations
    - Extract raw SQL seed helpers (create_account, create_user)
    - Restructure into typed groups (nil, integer, string) with hash pattern
    - Add DB cleanup in before hook to isolate from integration tests

--- spec/lib/incrementor_spec.rb
+++ spec/lib/incrementor_spec.rb
@@ -4,36 +4,84 @@ require "spec_helper"
 require "models/account"
 require "models/user"
 
-describe AutoIncrement::Incrementor do
-  {
-    nil => 1,
-    0 => 1,
-    1 => 2,
-    "A" => "B",
-    "Z" => "AA",
-    "AA" => "AB",
-    "AAAAA" => "AAAAB"
-  }.each do |previous_value, next_value|
-    describe "increment value for #{previous_value}" do
-      subject do
-        AutoIncrement::Incrementor.new User.new
-      end
+def create_account(code:, name: "seed")
+  conn = ActiveRecord::Base.connection
+  conn.execute "INSERT INTO accounts (name, code) VALUES (#{conn.quote(name)}, #{code})"
+end
 
-      it do
-        allow(subject).to receive(:maximum) { previous_value }
-        expect(subject.send(:increment)).to eq next_value
-      end
-    end
+def create_user(letter_code:, name: "seed")
+  conn = ActiveRecord::Base.connection
+  conn.execute "INSERT INTO users (name, letter_code) VALUES (#{conn.quote(name)}, #{conn.quote(letter_code)})"
+end
+
+describe AutoIncrement::Incrementor do
+  before do
+    Account.delete_all
+    User.delete_all
   end
 
-  describe "initial value of string" do
-    subject do
-      AutoIncrement::Incrementor.new User.new, initial: "A"
+  describe "#run" do
+    it "increments nil to 1" do
+      account = Account.new
+      AutoIncrement::Incrementor.new(account).run
+      expect(account.code).to eq 1
     end
 
-    it do
-      allow(subject).to receive(:maximum) { nil }
-      expect(subject.send(:increment)).to eq "A"
+    describe "integer" do
+      {
+        0 => 1,
+        1 => 2,
+        9 => 10
+      }.each do |previous_value, next_value|
+        it "increments #{previous_value} to #{next_value}" do
+          create_account(code: previous_value)
+
+          account = Account.new
+          AutoIncrement::Incrementor.new(account).run
+          expect(account.code).to eq next_value
+        end
+      end
+    end
+
+    describe "string" do
+      {
+        "A" => "B",
+        "Z" => "AA",
+        "AA" => "AB",
+        "AAAAA" => "AAAAB"
+      }.each do |previous_value, next_value|
+        it "increments #{previous_value} to #{next_value}" do
+          create_user(letter_code: previous_value)
+
+          user = User.new
+          AutoIncrement::Incrementor.new(user, column: :letter_code, initial: "A").run
+          expect(user.letter_code).to eq next_value
+        end
+      end
+    end
+
+    describe "when column value is already set" do
+      it "does not change the column if force is false" do
+        account = Account.new(code: 5)
+        expect { AutoIncrement::Incrementor.new(account).run }
+          .not_to change { account.code }
+      end
+
+      it "changes the column if force is true" do
+        account = Account.new(code: 5)
+        AutoIncrement::Incrementor.new(account, force: true).run
+        expect(account.code).not_to eq 5
+      end
+    end
+
+    describe "scoped increment" do
+      it "only considers records within the same scope" do
+        create_account(code: 10, name: "other")
+
+        account = Account.new(name: "mine")
+        AutoIncrement::Incrementor.new(account, scope: :name).run
+        expect(account.code).to eq 1
+      end
     end
   end
 
